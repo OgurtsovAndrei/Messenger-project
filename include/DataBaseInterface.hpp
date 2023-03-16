7f2a0e0379556ca24b945482a4f550c88855197f
Sir <brodloord@yandex.ru>
2023-03-16 20:35:34 +0300

    Start delete

--- include/DataBaseInterface.hpp
+++ include/DataBaseInterface.hpp
@@ -23,7 +23,7 @@ struct BDInterface {
     virtual Status change_user(const User &new_user) = 0;
     virtual Status
     get_user_log_pas(User &user) = 0;
-//    virtual Status del_user(const User &user) = 0;
+    virtual Status del_user(const User &user) = 0;
 //    virtual Status
 //    make_dialog_request(const User &from_user, const User &to_user) = 0;
 //    virtual Status
@@ -66,7 +66,7 @@ struct SQL_BDInterface : BDInterface {
     Status add_user(User &user);
     Status change_user(const User &new_user);
     Status get_user_log_pas(User &user);
-//    Status del_user(const User &user);
+    Status del_user(const User &user);
 //    Status make_dialog_request(const User &from_user, const User &to_user);
 //    Status close_dilog_request(const User &from_user, const User &to_user);
 //
