91d29fd23bdbcaab9923acf0dfdb9a5c3b354744
Sir <brodloord@yandex.ru>
2023-03-16 20:35:34 +0300

    Start delete

--- DataBaseInterface.cpp
+++ DataBaseInterface.cpp
@@ -73,10 +73,24 @@ Status SQL_BDInterface::get_user_log_pas(
     int exit = sqlite3_exec(m_bd, sql.c_str(), User::callback, 0, &message_error);
     chars_to_string(message_error, string_message);
     sqlite3_free(message_error);
-    return Status(exit == SQLITE_OK, "Problem in ADD Used.\nMessage: " + string_message + "\n SQL command: " + sql + "\n");
+    return Status(exit == SQLITE_OK, "Problem in GET Used.\nMessage: " + string_message + "\n SQL command: " + sql + "\n");
 }
 
-//Status SQL_BDInterface::del_user(const User &user);
+Status SQL_BDInterface::del_user(const User &user){
+    std::string sql =
+        "DELETE FROM Users WHERE ";
+    sql += "id=" + std::to_string(user.m_user_id) + " AND ";
+    sql += "Name='" + user.m_name + "' AND ";
+    sql += "Surname='" + user.m_surname + "' AND ";
+    sql += "Login='" + user.m_login + "' AND ";
+    sql += "PasswordHash='" + user.m_password_hash + "'";
+    char *message_error;
+    std::string string_message;
+    int exit = sqlite3_exec(m_bd, sql.c_str(), NULL, 0, &message_error);
+    chars_to_string(message_error, string_message);
+    sqlite3_free(message_error);
+    return Status(exit == SQLITE_OK, "Problem in DEL Used.\nMessage: " + string_message + "\n SQL command: " + sql + "\n");
+}
 //Status SQL_BDInterface::make_dialog_request(
 //    const User &from_user,
 //    const User &to_user
