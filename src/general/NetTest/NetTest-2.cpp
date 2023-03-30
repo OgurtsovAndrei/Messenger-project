24fcdfc38ade38ed2913fc20acfe92d59cf3e5a1
Ogurtsov_Andrey <avogurtsov@edu.hse.ru>
2023-03-30 22:06:35 +0300

    Add client->server : secured message Теперь и сервер умеет отправлять клиенту зашифрованные сообщения. Так же теперь сервер не пропускает первое сообщение, если оно не установка secured connection Больше см в изменении README в NetTest Оно показывает актуальный статус данной ветки

--- src/general/NetTest/NetTest-2.cpp
+++ src/general/NetTest/NetTest-2.cpp
@@ -7,14 +7,14 @@
 
 int main() {
     Net::Client::Client client("localhost", "12345");
-    client.make_connection();
-//    client.make_secure_connection();
-    for (int i = 0; i < 10; ++i) {
+//    client.make_connection();
+    client.make_secure_connection();
+    for (int i = 0; i < 3; ++i) {
         std::cout << "Iteration #" << i << "\n";
         client.send_text_message("Hi, that is iteration № " + std::to_string(i) + "!");
         client.get_request_and_out_it();
         client.send_secured_text_message("SECURED --> Secret hi, from iteration № " + std::to_string(i) + "!");
-        client.get_request_and_out_it();
+        client.get_secret_request_and_out_it();
         usleep(100'000);
     }
 }
