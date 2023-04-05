39be41125d272e3f64a60cc8ebddafa87e67c2d0
Arina Ivanova <arishkamu@gmail.com>
2023-04-05 22:28:10 +0300

    my first commt

---
+++ register.cpp
@@ -0,0 +1,37 @@
+#include "welcWindow.h"
+#include "register.h"
+#include "./ui_register.h"
+
+Register::Register(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Register)
+{
+    ui->setupUi(this);
+}
+
+Register::~Register()
+{
+    delete ui;
+}
+
+void Register::delRegInfo() {
+    ui->nameInput->close();
+    ui->nameLabel->close();
+    ui->snameInput->close();
+    ui->snameLabel->close();
+}
+
+void Register::on_cancelButton_clicked()
+{
+    WelcWindow *welc = new WelcWindow();
+    welc->show();
+    this->close();
+}
+
+
+void Register::on_readyButton_clicked()
+{
+    //TODO check if password okay
+    std::string a = ui->logInput->text().toStdString();
+}
+
