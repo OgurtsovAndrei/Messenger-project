f782b403fe5d52890766f1e9b089f52dd3dfb6a3
Sir <brodloord@yandex.ru>
2023-04-05 18:20:37 +0300

    make bd_connection in Server

--- include/database/Status.hpp
+++ include/database/Status.hpp
@@ -6,11 +6,21 @@
 namespace database_interface {
 
 struct Status {
+private:
     bool m_correct;
     std::string m_message;
 
+public:
     Status(bool correct, const std::string &message = "")
         : m_correct(correct), m_message(message) {
+    }
+
+    bool correct(){
+        return m_correct;
+    }
+
+    std::string message(){
+        return m_message;
     }
 };
 
--- src/general/NetTest/NetTest-1.cpp
+++ src/general/NetTest/NetTest-1.cpp
@@ -3,8 +3,12 @@
 //
 
 #include "NetServer.hpp"
+#include <cassert>
 
 int main() {
     Net::Server::Server server(12345);
+    assert(server.open_database());
     server.run_server(4);
+    assert(server.close_database());
+    return 0;
 }
