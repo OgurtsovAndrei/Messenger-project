79061d045d6ed4f8a4306708389c1c5eec9b3e6c
Sir <brodloord@yandex.ru>
2023-04-11 13:44:12 +0300

    Upgrade get_user_id_by_log

--- src/server/database/DB-main.cpp
+++ src/server/database/DB-main.cpp
@@ -7,8 +7,10 @@
 //int main(int argc, char **argv) {
 //    database_interface::SQL_BDInterface bd = database_interface::SQL_BDInterface();
 //    bd.open();
-//    database_interface::Dialog dialog("2", "RSA", 100000, 5, false);
-//    bd.make_dialog(dialog);
+//    database_interface::User user;
+//    user.m_login = "A-login";
+//    bd.get_user_id_by_log(user);
+//    std::cout << user.m_user_id << user.m_name << user.m_surname;
 //    bd.close();
 //    return (0);
 //}
