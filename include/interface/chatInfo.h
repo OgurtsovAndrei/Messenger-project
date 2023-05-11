1c10c8160fff56d0d7e01a6b3faaf6fc1724b095
Arina Ivanova <arishkamu@gmail.com>
2023-05-11 17:27:05 +0300

    add file

---
+++ include/interface/chatInfo.h
@@ -0,0 +1,27 @@
+#ifndef CHATINFO_H
+#define CHATINFO_H
+
+#include <QDialog>
+#include "Dialog.hpp"
+
+namespace Ui {
+class ChatInfo;
+}
+
+class ChatInfo : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ChatInfo(database_interface::Dialog *dialog, QWidget *parent = nullptr);
+    ~ChatInfo();
+
+private slots:
+    void on_addMemButton_clicked();
+
+private:
+    Ui::ChatInfo *ui;
+    database_interface::Dialog *dialog;
+};
+
+#endif // CHATINFO_H
