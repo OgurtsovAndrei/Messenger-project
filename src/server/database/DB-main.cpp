14d8957b4b43a2e83cd3b62429028d5c194bd503
Sir <brodloord@yandex.ru>
2023-05-14 15:48:56 +0300

    Fix bd problrm

--- src/server/database/DB-main.cpp
+++ src/server/database/DB-main.cpp
@@ -1,19 +1,20 @@
-//#include <sqlite3.h>
-//#include <iostream>
-//#include <list>
-//#include <chrono>
-//#include <ctime>
-//#include "../../../include/database/DataBaseInterface.hpp"
-//#include "User.hpp"
-//
-//int main(int argc, char **argv) {
-//    database_interface::SQL_BDInterface bd = database_interface::SQL_BDInterface();
-//    bd.open();
-//    database_interface::User user;
-//    user.m_login = "A";
-//    user.m_password_hash = "A-password";
-//    std::cout << bd.get_user_by_log_pas(user).correct() << bd.get_user_by_log_pas(user).message() << '\n';
-//    std::cout << '<' << user.m_user_id << '|' << user.m_name << '|' << user.m_surname << '|' << user.m_login << '|' << user.m_password_hash << ">\n";
-//    bd.close();
-//    return (0);
-//}
+#include <sqlite3.h>
+#include <iostream>
+#include <list>
+#include <chrono>
+#include <ctime>
+#include "DataBaseInterface.hpp"
+#include "User.hpp"
+
+int main(int argc, char **argv) {
+    database_interface::SQL_BDInterface bd = database_interface::SQL_BDInterface();
+    std::cout << bd.open().correct() << '\n';
+    std::string sql = "INSERT INTO Users (Name, Surname, Login, PasswordHash) VALUES ('q', 'q', 'q2', 'ok');";
+    char *message_error;
+    std::string string_message;
+    int exit = sqlite3_exec(bd.m_bd, sql.c_str(), NULL, 0, &message_error);
+    std::cout << (exit == SQLITE_OK) << ' ' << message_error << '\n';
+    sqlite3_free(message_error);
+    std::cout << bd.close().correct() << '\n';
+    return (0);
+}
