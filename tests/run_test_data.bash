6d1b3598754053baa1c90e07056386fca90de2ce
Ogurtsov_Andrey <avogurtsov@edu.hse.ru>
2023-05-24 21:40:50 +0300

    Make smart `run_all_test.bash` file

--- tests/run_test_data.bash
+++
@@ -1,8 +0,0 @@
-# bin/bash
-
-cxxtestgen --error-printer -o made-testes.cpp ./*.hpp
-g++-12 made-testes.cpp -o tests -std=c++20
-./tests
-
-
-#-l:libbotan-3.a -I/usr/local/include/botan-3 -I./../include -I./../include/database -lsqlite3
