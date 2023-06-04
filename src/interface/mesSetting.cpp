6d227c4d51d6c248a66f6e2c6025c5e33db47914
Arina Ivanova <arishkamu@gmail.com>
2023-06-05 00:57:58 +0300

    add some features

--- src/interface/mesSetting.cpp
+++ src/interface/mesSetting.cpp
@@ -1,9 +1,10 @@
 #include "interface/mesSetting.h"
 #include "interface/welcWindow.h"
+#include "interface/bubble.h"
 #include "ui_mesSetting.h"
 
-MesSetting::MesSetting(QListWidgetItem *mes, MainWindow *mainWin, QWidget *parent) :
-    mes(mes), mainWin(mainWin),
+MesSetting::MesSetting(QListWidgetItem *msg, MainWindow *mainWin, QWidget *parent) :
+    msg(msg), mainWin(mainWin),
     QWidget(parent),
     ui(new Ui::MesSetting)
 {
@@ -20,15 +21,32 @@ MesSetting::~MesSetting()
 
 void MesSetting::on_delButton_clicked()
 {
-    auto status = client.delete_message(mes->type());
+    auto status = client.delete_message(dynamic_cast<Bubble*>(msg)->get_msg_id());
     close();
 }
 
 
 void MesSetting::on_editButton_clicked()
 {
-    mainWin->set_change_msg_is(mes->type());
-    mainWin->change_message(mes);
+    mainWin->set_change_msg_is(dynamic_cast<Bubble*>(msg)->get_msg_id());
+    mainWin->change_message(msg);
     close();
 }
 
+void MesSetting::on_downloadButton_clicked()
+{
+    mainWin->set_change_msg_is(dynamic_cast<Bubble*>(msg)->get_msg_id());
+    mainWin->change_message(msg);
+    close();
+}
+
+
+void MesSetting::close_downoload() {
+    ui->downloadButton->close();
+    setFixedHeight(60);
+}
+
+void MesSetting::close_edit() {
+    ui->editButton->close();
+    setFixedHeight(60);
+}
